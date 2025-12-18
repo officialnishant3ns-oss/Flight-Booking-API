cc4d2733ed8a0d0081421aaba352653f13fee152
NISHANT SINGH <official.nishant.3ns@gmail.com>
2025-12-18 19:26:36 +0530

    added database connection

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import app from '../src/app.js'
 import dotenv from "dotenv"
+import connectdb from './db/db.js'
 dotenv.config({
     path: './.env'
 })
+
+connectdb()
 
 app.listen(process.env.PORT ||4000, () => {
     console.log(`Server is ready at ${process.env.PORT}`)
